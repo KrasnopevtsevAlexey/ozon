d72117fff8ce5757e90f55ba7eb2b9a3c52b03dc
Alexsey779 <fktrctq321@gmail.com>
2025-11-23 11:02:40 +0700

    first

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 // import type { NextConfig } from "next";
 
-import { NextConfig } from "next";
+// import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  productionBrowserSourceMaps: false,
-  turbopack: {}
-};
+// const nextConfig: NextConfig = {
+//   productionBrowserSourceMaps: false,
+//   turbopack: {}
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ozon-next' : '',
+}
+
+module.exports = nextConfig
